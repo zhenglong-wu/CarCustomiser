c0251bfe1fab97fe15dc964150eec7962cadaab5
Zhenglong Wu <wu.z@etoncollege.org.uk>
2021-01-20 10:57:41 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Zhenglong Wu on 20/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarCustomiser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarCustomiser
+//
+//  Created by Zhenglong Wu on 20/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
