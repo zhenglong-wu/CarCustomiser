6d72e1a9a82727c239d1a6fe8e705f691a916a33
Zhenglong Wu <wu.z@etoncollege.org.uk>
2021-01-21 10:18:06 +0000

    CANVAS SwiftUI Car Customiser Task 1 complete

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -12,9 +12,31 @@ struct ContentView: View {
     let car = Car(make: "Porsche", model: "718 Boxster", topSpeed: 185, acceleration: 3.9, handling: 7)
     
     var body: some View {
-        Text("Car Make: \(car.make)")
-            .padding()
+        
+        ZStack(alignment: .top) {
+                
+            HStack {
+                        
+                    VStack(alignment: .leading) {
+                        
+                        Text("Make: \(car.make)")
+                        Text("Model: \(car.model)")
+                        Text("Top speed: \(car.topSpeed)mph")
+                        Text("Acceleration (0-60): \(car.acceleration)s")
+                        Text("Handling: \(car.handling)")
+                                
+                    }
+                    .font(.system(size: 15))
+                    .padding(35)
+                }
+            .frame(minWidth: 150, maxHeight: .infinity, alignment: .topLeading)
+                
+
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading)
+
     }
+                
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -22,4 +44,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-     
+
+
+
